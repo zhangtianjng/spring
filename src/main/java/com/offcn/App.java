8ab871cd8e8b06dfebbc1e146913926daf76054a
张田静 <573621169@qq.com>
2020-07-10 11:55:55 +0800

    初始化项目

---
+++ src/main/java/com/offcn/App.java
@@ -0,0 +1,14 @@
+package com.offcn;
+
+import javax.imageio.stream.ImageInputStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println();
+        for (int i = 0; i <3; i++) {
+        }
+    }
+}
